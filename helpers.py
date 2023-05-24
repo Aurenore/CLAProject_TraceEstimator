ef4d5196a77ad7ce02e4fb5b964664db9ed580b6
Anya M <anya-aurore.mauron@epfl.ch>
2023-05-24 20:31:00 +0200

    changed ylabel

--- helpers.py
+++ helpers.py
@@ -516,7 +516,7 @@ def graph_algo1(A, u, matrix_label, savefile, maxit=100, epsilon=1e-5):
     plt.plot(Ms, L_p[:j], '-.')
     plt.legend(['exact value', 'upper bound', 'lower bound'])
     plt.xlabel('Number of iteration')
-    plt.ylabel(r'$\log(u^\top A^{-1} u)$')
+    plt.ylabel(r'$u^\top A^{-1} u$')
     plt.title(matrix_label +' (' + str(n) + 'x' + str(n)+')') 
     plt.yscale('log')
     plt.savefig('figures/' +'algo1_'+ savefile +'.png', bbox_inches='tight')
